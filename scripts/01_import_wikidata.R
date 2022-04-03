2c08b964c9324aeee9a0223de82e21765878fd42
Ethan Sansom <ethan.sansom29@gmail.com>
2022-04-03 10:48:47 -0400

    Edit import_wikidata script.

--- scripts/01_import_wikidata.R
+++ scripts/01_import_wikidata.R
@@ -1,10 +1,22 @@
-library(tidyverse)
-library(WikidataQueryServiceR)
+# Preamble ---------------------------------------------------------------------
+# Purpose: Import the full dataset of Canadian Politicians available on Wikidata
+# who have a recorded political position and political party. Retrieve additional
+# optional variables for politicians' year of birth/death and gender.
+# Author: Ethan Sansom
+# Contact: ethan.sansom@mail.utotoronto.ca
+# Date: 2022-04-03
 
-# Define the SPARQL query (for the Wikidata Query Service API) which returns all Canadian Politicians
+# Load Packages ----------------------------------------------------------------
+library(tidyverse)
+library(WikidataQueryServiceR)  # For accessing Wikidata Query Service API
+library(here)                   # For file path management
+
+#  Import Data -----------------------------------------------------------------
+# Define the SPARQL (Sparkle) query for the Wikidata Query Service API, which
+# returns the desired set of Canadian Politicians
 query <-
   '# Return the Wikidata ID, Name, Political Position(s), Gender, Birthdate (Year), 
-   #Deathdate (Year), and Political Party(s) of all Canadian Politicians in the Wikidata Database
+   # Deathdate (Year), and Political Party(s) of all Canadian Politicians in the Wikidata Database
    SELECT ?polit ?politLabel ?positionLabel ?genderLabel (year(?birthdate) as ?birthyear) (year(?deathdate) as ?deathyear) ?partyLabel
    WHERE 
    {
@@ -27,4 +39,4 @@ query <-
 wiki_data_raw <- query_wikidata(query)
 
 # Save the dataset
-write_csv(x = wiki_data_raw, file = "/Users/ethansansom/Desktop/wiki_data_raw.csv")
+write_csv(x = wiki_data_raw, file = here("inputs/data/wiki_data_raw.csv"))
