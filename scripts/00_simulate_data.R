1f7a58a9803d83136fd26f2fc46ab07a99a81f4b
Ethan Sansom <ethan.sansom29@gmail.com>
2022-04-28 07:23:44 -0400

    Clean and comment scripts for final submission

--- scripts/00_simulate_data.R
+++ scripts/00_simulate_data.R
@@ -5,21 +5,18 @@ library(ggplot2)         # Plot Making
 library(ggrepel)         # Adding Point Labels
 library(scales)          # For formatting plot axes
 
-library(ggbeeswarm)      # For NYT Style Bee-swarm Plots
-library(packcircles)     # For NYT Style Bee-swarm Plots
-
 set.seed(1)
 
 # Simulate Data ----------------------------------------------------------------
 
 # Expected Variable Factors
 parties <- c("NDP", "Liberal", "Green", "CPC", "BQ")
-positions <- c("Other", "Minister", "Premier", "House", "Senate")
+positions <- c("Other", "Minister", "Prime Minister", "Party Leader", "Minister")
 
 # Set Simulation Parameters
 sample_size <- 100
-mean_age <- 60
-sd_age <- 10
+mean_birthyear <- 1900
+sd_birthyear <- 40
 
 # Simulate Data
 df <-
@@ -27,10 +24,8 @@ df <-
     id = 1:sample_size,
     party = sample(x = parties, size = sample_size, replace = TRUE),
     position = sample(x = positions, size = sample_size, replace = TRUE),
-    sex = sample(x = c("Male", "Female"), size = sample_size, replace = TRUE),
-    age = rnorm(n = sample_size, mean = mean_age, sd = sd_age),
-    page_views = sample(x = 0:1000000, size = sample_size, replace = TRUE),
-    revert_percent = sample(x = 1:50, size = sample_size, replace = TRUE)
+    birthyear = rnorm(n = sample_size, mean = mean_birthyear, sd = sd_birthyear),
+    page_views = sample(x = 0:1000000, size = sample_size, replace = TRUE)
   )
 
 # Add Anticipated Prime Minister Outliers
@@ -39,7 +34,6 @@ df$page_views[pm_index] <- df$page_views[pm_index] + 1000000
 df$position[pm_index] <- "Prime Minister"
 
 #  Produce Plots ---------------------------------------------------------------
-
 # Map Political Parties to Appropriate Colors
 party_colour = 
   c(
@@ -75,9 +69,7 @@ ggplot(data = df_plot, aes(x = party, y = page_views, label = id), position = po
   # Label Points Belonging to Prime Ministers
   geom_text_repel(
     position = pos,
-    #nudge_x = 0.25,
     box.padding = 1,
-    #nudge_y = 5000,
     segment.curvature = 0.5,
     segment.ncp = 5,
     segment.angle = 90
@@ -96,7 +88,8 @@ ggplot(data = df_plot, aes(x = party, y = page_views, label = id), position = po
   theme_minimal() +
   scale_color_manual(values = party_colour)
 
-# Plot of Proportion of Page Views, Pages, and Actual Seats by Party
+# Plot of Proportion of Page Views, Pages, and Actual Seats by Party -----------
+# Summarize party page views and page counts
 party_proportions <-
   df |>
   group_by(party) |>
@@ -106,6 +99,7 @@ party_proportions <-
   ) |>
   mutate(seats = sample(x = 1:100, size = 5))
 
+# Convert party page views and page counts to proportions, add actual election data
 party_percents <-
   party_proportions |>
   mutate(
@@ -119,12 +113,11 @@ party_percents <-
   party_percents |>
   pivot_longer(!party, names_to = "type", values_to = "percent")
 
-
+# Create plot
 party_percents |>
-  
-  # Create Plot
   ggplot(aes(x = percent, y = type, fill = party)) +
   geom_bar(stat = "identity", position = "stack") +
+  
   # Text Labels with help from:
   # https://stackoverflow.com/questions/34903368/how-to-center-stacked-percent-barchart-labels
   geom_text(
@@ -145,88 +138,13 @@ party_percents |>
     fill = "Party"
   ) +
   
+  # Set scales
   scale_x_continuous(labels = percent_format()) +
   
   # Set Theme and Color Scheme
-  # theme_classic() +
   theme_minimal() +
-  
   theme(
     panel.grid.major.y = element_blank(),
     panel.grid.minor = element_blank()
   ) +
-
   scale_fill_manual(values = party_colour)
-
-# Sort of Works for Beeswarm Plot - Can do better?
-set.seed(10)
-rand_points <- data.frame(x_group = "group",
-                          x=rnorm(200, mean=50, sd=10),
-                          size=rexp(200, 1))
-
-beeswarm_without_size <- rand_points %>%
-  ggplot(aes(x=x, y=x_group)) +
-  geom_beeswarm(groupOnX=FALSE)
-
-oldbee_chart_data <- ggplot_build(beeswarm_without_size)
-
-newbee_frame <- data.frame(x=oldbee_chart_data$data[[1]]$x, 
-                           y=oldbee_chart_data$data[[1]]$y, 
-                           r=rand_points$size)
-
-newbee_repel <- circleRepelLayout(newbee_frame, wrap=FALSE)
-
-newbee_repel_out <- circleLayoutVertices(newbee_repel$layout, xysizecols = 1:3)
-
-newbee_repel_out %>% ggplot(aes(x, y, group=id)) +
-  # Altering the SIZE Parameter can adjust for slight overlap
-  geom_polygon(aes(color=id, fill=id), size = 0.15) +
-  coord_equal() +
-  labs(x="X value", y="",
-       title="This is a Bee Swarm Chart in R") +
-  theme_classic() #+ theme(legend.position = "none")
-
-
-# Bubble Plot
-df <- df |> rename("ident" = id)
-beeswarm_without_size <- 
-  df |>
-  # If x-value is too large -> can't adjust the circle sizes
-  # Have to scale x-value, then RE-SCALE AXIS Labels
-  ggplot(aes(x=page_views/10000, y="group", colour = party)) +
-  geom_beeswarm(groupOnX=FALSE)
-
-oldbee_chart_data <- ggplot_build(beeswarm_without_size)
-
-newbee_frame <- data.frame(x=oldbee_chart_data$data[[1]]$x, 
-                           y=oldbee_chart_data$data[[1]]$y,
-                           r=df$revert_percent
-                           )
-
-newbee_repel <- circleRepelLayout(newbee_frame, wrap=FALSE, maxiter = 10000)
-
-newbee_repel_out <- circleLayoutVertices(newbee_repel$layout, xysizecols = 1:3)
-newbee_repel_out <- cbind(newbee_repel_out, df[newbee_repel_out$id, ])
-  
-# circleLayoutVertices: IDs will be the row numbers of the input circle data
-
-newbee_repel_out %>% ggplot(aes(x, y, group=id)) +
-  # Altering the SIZE Parameter can adjust for slight overlap
-  geom_polygon(aes(fill=party, colour = party), alpha = 0.7) +
-  coord_equal() +
-  labs(
-    x = "X value", 
-    y = "",
-    title = "Bee Swarm Plot",
-    subtitle = "Circle Size represents reverted edit percentage.",
-    fill = "Party",
-    colour = "Party"
-    ) +
-  theme_classic() +
-  # Re-scale Labels
-  scale_x_continuous(labels = ~ format(. * 10000, scientific = FALSE)) +
-  scale_fill_manual(values = party_colour) +
-  scale_colour_manual(values = party_colour)
-
-
-
--- scripts/04_clean_pagedata.R
+++ scripts/04_clean_pagedata.R
@@ -2,3 +2,157 @@
 # Author: Ethan Sansom
 # Contact: ethan.sansom@mail.utotoronto.ca
 # Date: 2022-04-03
+
+# Load Packages ----------------------------------------------------------------
+library(tidyverse)
+library(here)           # For file path management
+
+# Load and Clean Page Data -----------------------------------------------------
+wiki_data <- read_csv(here("inputs/data/wiki_data_pages.csv"))
+
+# Get the total page views for each politician
+wiki_data <-
+  wiki_data |>
+  rowwise() |>
+  mutate(page_views = sum(across(starts_with("views"), na.rm = TRUE)))
+
+# Find politicians which have been assigned multiple party affiliations
+multi_party_polits <-
+  wiki_data |>
+  mutate(num_parties = cp + liberal + green + ndp + bq) |>
+  filter(num_parties > 1) |>
+  select(liberal, cp, ndp, green, ndp, name, qid, url)
+
+# Assign each politician in multi_party_polits to their most recent party
+# NOTE: There are 81 such politicians of the 7856 in the sample, for which I have
+# manually altered their affiliation. In future analysis, with a different sample,
+# you may wish to simply drop these politicians or accept multi-party affiliation.
+
+party_map <-
+  c(
+    "https://en.wikipedia.org/wiki/Robert_Borden" = "cp",
+    "https://en.wikipedia.org/wiki/Mackenzie_Bowell" = "cp",
+    "https://en.wikipedia.org/wiki/John_A._Macdonald" = "cp",
+    "https://en.wikipedia.org/wiki/James_Alexander_Calder" = "cp",
+    "https://en.wikipedia.org/wiki/Robert_James_Manion" = "cp",
+    "https://en.wikipedia.org/wiki/William_Thomas_White" = "cp",
+    "https://en.wikipedia.org/wiki/William_Evan_Price" = "cp",
+    "https://en.wikipedia.org/wiki/Wajid_Khan_(Canadian_politician)" = "cp",
+    "https://en.wikipedia.org/wiki/Sylvester_Perry_Ryan" = "cp",
+    "https://en.wikipedia.org/wiki/Stephen_Harper" = "cp",
+    "https://en.wikipedia.org/wiki/Stan_Woloshyn" = "cp",
+    "https://en.wikipedia.org/wiki/Scott_Brison" = "liberal",
+    "https://en.wikipedia.org/wiki/Sandra_Jansen" = "ndp",
+    "https://en.wikipedia.org/wiki/Robert_Muir_(politician)" = "cp",
+    "https://en.wikipedia.org/wiki/Robert_Lanct%C3%B4t" = "bq",
+    "https://en.wikipedia.org/wiki/Rick_Laliberte" = "liberal",
+    "https://en.wikipedia.org/wiki/Richard_John_Cartwright" = "liberal",
+    "https://en.wikipedia.org/wiki/Raymond_Rock" = "cp",
+    "https://en.wikipedia.org/wiki/Raymond_Cho_(politician)" = "liberal",
+    "https://en.wikipedia.org/wiki/Rathika_Sitsabaiesan" = "liberal",
+    "https://en.wikipedia.org/wiki/Ralph_Stewart_(Canadian_politician)" = "cp",
+    "https://en.wikipedia.org/wiki/Raj_Sherman" = "liberal",
+    "https://en.wikipedia.org/wiki/Pierrette_Venne" = "bq",
+    "https://en.wikipedia.org/wiki/Pierre_Nantel" = "green",
+    "https://en.wikipedia.org/wiki/Peter_Ittinuar" = "green",
+    "https://en.wikipedia.org/wiki/Pauline_Jewett" = "ndp",
+    "https://en.wikipedia.org/wiki/Paul_Hellyer" = "liberal",
+    "https://en.wikipedia.org/wiki/Nic_Leblanc" = "cp",
+    "https://en.wikipedia.org/wiki/Nancy_MacBeth" = "liberal",
+    "https://en.wikipedia.org/wiki/Mo%C3%AFse_Houde" = "cp",
+    "https://en.wikipedia.org/wiki/Maria_Mourani" = "bq",
+    "https://en.wikipedia.org/wiki/Lucien_Bouchard" = "bq",
+    "https://en.wikipedia.org/wiki/Louis_Plamondon" = "bq",
+    "https://en.wikipedia.org/wiki/Lise_St-Denis" = "liberal",
+    "https://en.wikipedia.org/wiki/Lillian_Dyck" = "liberal",
+    "https://en.wikipedia.org/wiki/Lex_MacKenzie" = "cp",
+    "https://en.wikipedia.org/wiki/Julius_Yankowsky" = "cp",
+    "https://en.wikipedia.org/wiki/John_Sandfield_Macdonald" = "cp",
+    "https://en.wikipedia.org/wiki/John_Loney" = "liberal",
+    "https://en.wikipedia.org/wiki/John_Crosbie" = "cp",
+    "https://en.wikipedia.org/wiki/Joe_Comuzzi" = "liberal",
+    "https://en.wikipedia.org/wiki/Joan_Beatty" = "liberal",
+    "https://en.wikipedia.org/wiki/Jean_Rousseau_(politician)" = "green",
+    "https://en.wikipedia.org/wiki/Jean_Lapierre" = "liberal",
+    "https://en.wikipedia.org/wiki/James_Domville" = "liberal",
+    "https://en.wikipedia.org/wiki/Jacques_Lavoie" = "liberal",
+    "https://en.wikipedia.org/wiki/Jack_Horner_(politician)" = "liberal",
+    "https://en.wikipedia.org/wiki/Hunter_Tootoo" = "liberal",
+    "https://en.wikipedia.org/wiki/Hugh_Guthrie" = "cp",
+    "https://en.wikipedia.org/wiki/Honor%C3%A9_Achim" = "liberal",
+    "https://en.wikipedia.org/wiki/Hiram_Blanchard" = "liberal",
+    "https://en.wikipedia.org/wiki/Guy_St-Julien" = "liberal",
+    "https://en.wikipedia.org/wiki/Giorgio_Mammoliti" = "cp",
+    "https://en.wikipedia.org/wiki/Gilles_Rocheleau" = "bq",
+    "https://en.wikipedia.org/wiki/Gilbert_Chartrand" = "cp",
+    "https://en.wikipedia.org/wiki/George_Valentine_McInerney" = "cp",
+    "https://en.wikipedia.org/wiki/Gaston_Gourde" = "bq",
+    "https://en.wikipedia.org/wiki/Gary_Carr_(politician)" = "liberal",
+    "https://en.wikipedia.org/wiki/Garth_Turner" = "liberal",
+    "https://en.wikipedia.org/wiki/Fran%C3%A7oise_Boivin" = "ndp",
+    "https://en.wikipedia.org/wiki/Fran%C3%A7ois_G%C3%A9rin" = "bq",
+    "https://en.wikipedia.org/wiki/Elijah_Harper" = "liberal",
+    "https://en.wikipedia.org/wiki/Edmund_Burke_Wood" = "liberal",
+    "https://en.wikipedia.org/wiki/Diane_St-Jacques" = "liberal",
+    "https://en.wikipedia.org/wiki/David_Price_(Canadian_politician)" = "liberal",
+    "https://en.wikipedia.org/wiki/David_Kilgour" = "liberal",
+    "https://en.wikipedia.org/wiki/David_Emerson" = "cp",
+    "https://en.wikipedia.org/wiki/Claude_Patry" = "bq",
+    "https://en.wikipedia.org/wiki/Charles_Bruno_Blondeau" = "cp",
+    "https://en.wikipedia.org/wiki/Bud_Miller" = "cp",
+    "https://en.wikipedia.org/wiki/Bruce_Hyer" = "ndp",
+    "https://en.wikipedia.org/wiki/Bob_Rae" = "liberal",
+    "https://en.wikipedia.org/wiki/Blair_Wilson" = "liberal",
+    "https://en.wikipedia.org/wiki/Bill_Matthews" = "liberal",
+    "https://en.wikipedia.org/wiki/Bill_Dickie_(politician)" = "cp",
+    "https://en.wikipedia.org/wiki/Beno%C3%AEt_Tremblay" = "bq",
+    "https://en.wikipedia.org/wiki/Belinda_Stronach" = "liberal",
+    "https://en.wikipedia.org/wiki/Angela_Vautour" = "cp",
+    "https://en.wikipedia.org/wiki/Andrew_B._Ingram" = "cp",
+    "https://en.wikipedia.org/wiki/Andr%C3%A9_Harvey" = "liberal",
+    "https://en.wikipedia.org/wiki/Alexa_McDonough" = "ndp"
+  )
+
+# Get the urls corresponding to each party assignment
+party_map_urls <- names(party_map)
+
+# Recode the mulit-party politician's party indicators
+for (polit_url in party_map_urls) {
+  # Replace every party affiliation indicator of multi-party politicians with 0
+  index <- which(wiki_data$url == polit_url)
+  wiki_data[index, ][c("liberal", "ndp", "cp", "green", "bq")] <- 0
+  
+  # Assign a 1 to the politician's new assigned party affiliation
+  new_party <- party_map[polit_url]
+  wiki_data[index, ][new_party] <- 1
+}
+
+# Create a party factor variable
+wiki_data <-
+  wiki_data |>
+  mutate(
+    party = case_when(
+      cp == 1 ~ "CPC",
+      liberal == 1 ~ "Liberal",
+      green == 1 ~ "Green",
+      ndp == 1 ~ "NDP",
+      bq == 1 ~ "BQ"
+    )
+  )
+
+# Save Cleaned Data ------------------------------------------------------------
+# Select desired variables
+wiki_data <-
+  wiki_data |>
+  select(-c(polit, page_name, page_lang, url))
+
+# Test that the number of mulit-party politicians in now 0
+multi_party_polits <-
+  wiki_data |>
+  mutate(num_parties = cp + liberal + green + ndp + bq) |>
+  filter(num_parties > 1)
+
+print(c("0 Remaining Multi-Party Politicians:", dim(multi_party_polits)[1] == 0))
+
+# Save data
+write_csv(x = wiki_data, file = here("inputs/data/wiki_data_pages_clean.csv"))
